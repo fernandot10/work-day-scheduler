7aa69e259866bfdfba7f905d74577d21750aaeed
Fernando <fernandotrujillo@Fernandos-MBP.attlocal.net>
2022-11-19 13:59:29 -0500

    js set input and row to ls then getitem

--- Develop/script.js
+++ Develop/script.js
@@ -2,28 +2,28 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 // $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
+    // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  var textInput = document.getElementById('textInput');
-  var saveButton = document.getElementById('save');
+    $('.saveBtn').on('click', function(){
 
-  saveButton.addEventListener('click', save);
+      var textInput = $(this).siblings(".description").val();
+      var timeRow = $(this).parent().attr("id");
 
-  textInput.value = localStorage.getItem('textInput');
-
-  function save(){
-    var value = textInput.value;
-    localStorage.setItem('textInput',value);
-  }
-
-//   inputSave.addEventListener('click',event => {
-//   localStorage.setItem('notes', ) 
-//  })
-
+      localStorage.setItem(timeRow, textInput);
+  })
+  $("#9  .description").val(localStorage.getItem("9"));
+  $("#10 .description").val(localStorage.getItem("10"));
+  $("#11 .description").val(localStorage.getItem("11"));
+  $("#12 .description").val(localStorage.getItem("12"));
+  $("#13 .description").val(localStorage.getItem("13"));
+  $("#14 .description").val(localStorage.getItem("14"));
+  $("#15 .description").val(localStorage.getItem("15"));
+  $("#16 .description").val(localStorage.getItem("16"));
+  $("#17 .description").val(localStorage.getItem("17"));
 
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
